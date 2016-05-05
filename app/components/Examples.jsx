348ed1665aab722baea57e13e2d801115e24bdb9
Jonathan Dawson <jonathan@akavy.com>
2016-05-05 14:45:25 +0100

    Small change to Examples for pushing test to Github

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 var Examples = (props) => {
   return (
-    <h3>Examples compontent</h3>
+    <h3>Examples compontent!</h3>
+    <p>This is where the change is happening</p>
   );
 };
 
