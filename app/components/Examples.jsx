06fd5456e9aec05b35bc29a433828d0a0f999983
Jonathan Dawson <jonathan@akavy.com>
2016-05-06 13:20:42 +0100

    Added foundation-sites

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 var Examples = (props) => {
   return (
-    <h3>Examples compontent!</h3>
-    <p>This is where the change is happening</p>
+    <div>
+      <h3>Examples compontent!</h3>
+      <p>This is where the change is happening</p>
+    </div>
   );
 };
 
